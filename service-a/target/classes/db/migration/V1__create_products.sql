6ec3cf6a939be1be9a6852e505ebb373e0573ce2
Nguyen Doan Trong Doanh <nguyen.trong-doanh@hdsaison.com.vn>
2026-04-13 13:36:28 +0700

    remove target folders

--- service-a/target/classes/db/migration/V1__create_products.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price_cents BIGINT NOT NULL,
-    version BIGINT NOT NULL DEFAULT 0
-);
--- service-a/target/classes/db/migration/V2__seed_demo_product.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO products (id, name, description, price_cents, version)
-VALUES (1, 'Demo keyboard', 'Mechanical, hot-swappable', 1299900, 0);
-
-SELECT setval(pg_get_serial_sequence('products', 'id'), (SELECT MAX(id) FROM products));
